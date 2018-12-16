a9bb39758421ebb10973f2ee8058622761dfdb76
Joey <joey.xf@gmail.com>
2018-12-16 10:55:07 +0800

    加上简单的测试用例
    还没搞定生命周期部分，目前无法顺序执行每个task

--- examples/demo.rs
+++ examples/demo.rs
@@ -2,43 +2,47 @@ extern crate flow_rs;
 
 use flow_rs::process_def::ProcessDef;
 use flow_rs::process_ins::ProcessIns;
-use flow_rs::task::Task;
+// use flow_rs::task::Task;
 
 fn main() {
-    let mut proc_def = ProcessDef::new("简单的测试流程".to_string(), "process_definition_1".to_string());
-    let task1 = create_task1();
-    let task2 = create_task2();
-    let task3 = create_task3();
-    proc_def.add_task(&task1);
-    proc_def.add_task(&task2);
-    proc_def.add_task(&task3);
+    let proc_def = ProcessDef::new(
+        "简单的测试流程".to_string(),
+        "process_definition_1".to_string(),
+    );
+    // let task1 = create_task1();
+    // let task2 = create_task2();
+    // let task3 = create_task3();
 
-    proc_def.link_task(&task1);
-    proc_def.link_task(&task2);
-    proc_def.link_task(&task3);
+    // let task11 = &task1;
+    // proc_def.add_task(task11);
+    // proc_def.add_task(&task2);
+    // proc_def.add_task(&task3);
 
-    let proc_ins = ProcessIns::new(proc_def);
+    // proc_def.link_task(&task1);
+    // proc_def.link_task(&task2);
+    // proc_def.link_task(&task3);
+
+    let proc_ins = ProcessIns::new(&proc_def);
     proc_ins.run();
 }
 
-fn create_task1() -> Task {
-    return Task::new("任务1".to_string(), run_task1);
-}
+// fn create_task1() -> Task {
+//     return Task::new("任务1".to_string(), run_task1);
+// }
+// fn run_task1() {
+//     println!("正在运行任务1");
+// }
 
-fn run_task1() {
-    println!("正在运行任务1");
-}
+// fn create_task2() -> Task {
+//     return Task::new("任务2".to_string(), run_task2);
+// }
+// fn run_task2() {
+//     println!("正在运行任务2");
+// }
 
-fn create_task2() -> Task {
-    return Task::new("任务2".to_string(), run_task2);
-}
-fn run_task2() {
-    println!("正在运行任务2");
-}
-
-fn create_task3() -> Task {
-    return Task::new("任务3".to_string(), run_task3);
-}
-fn run_task3() {
-    println!("正在运行任务3");
-}
+// fn create_task3() -> Task {
+//     return Task::new("任务3".to_string(), run_task3);
+// }
+// fn run_task3() {
+//     println!("正在运行任务3");
+// }
--- src/process_ins.rs
+++ src/process_ins.rs
@@ -1,12 +1,12 @@
 use crate::process_def::ProcessDef;
 
-pub struct ProcessIns {
+pub struct ProcessIns<'a> {
     pub id: String,
-    pub process_def: ProcessDef,
+    pub process_def: &'a ProcessDef<'a>,
 }
 
-impl ProcessIns {
-    pub fn new(def: ProcessDef) -> Self {
+impl<'a> ProcessIns<'a> {
+    pub fn new(def: &'a ProcessDef<'a>) -> Self {
         return ProcessIns {
             id: "process instalce id".to_string(),
             process_def: def,
@@ -15,8 +15,10 @@ impl ProcessIns {
 
     pub fn run(&self) {
         let tasks = self.process_def.seq.iter();
+        println!("正在运行流程{}", self.id);
         for task in tasks {
             task.run();
         }
+        println!("流程{}结束", self.id);
     }
 }
